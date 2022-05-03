e9eb2565441cc32998dd090a375a0f10a8daeafd
Gabriel Barros Feitosa Sá <gabreilbarros13@gmail.com>
2022-05-02 23:22:05 -0300

    Update

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -6,14 +6,23 @@ const Button = (props) => {
   const convertMethod = props.action[0];
   const identifier = props.action[1];
   function execute() {
+    if (props.copy === true) {
+      Action.copy(identifier);
+      return 0;
+    } else if (props.clear === true) { 
+      Action.clear(identifier);
+      return 0;
+    }
+    
     Action.execute(convertMethod, identifier);
-  }
-  
+  } 
+
   return (
     <button 
       className={ styles.button } 
       onClick={ execute }
     >
+    {props.icon && <props.icon size={20}/>}
     { props.children }
     </button>
   );
