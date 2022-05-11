0de390f519bf209696228711bd1707e33ac85cf8
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-05-11 20:43:35 -0300

    Adicionando textos na página

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={ styles.footer }><div>COPYRIGHT &copy;</div> <div>Powered by  VELLOWARE</div></footer>
+  );
+};
+
+export default Footer;
